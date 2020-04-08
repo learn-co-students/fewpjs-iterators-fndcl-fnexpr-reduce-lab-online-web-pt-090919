85d4368c589b056af99ada8a2d2fcc4b043aed2c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-08 18:01:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function totalBatteries(){
+  let total = 0;
+  return batteryBatches.reduce(function(total, element){
+    return total += element
+  })
+}
